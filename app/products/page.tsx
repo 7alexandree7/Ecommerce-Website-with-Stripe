6451272a2e8ca6dfb3b2a654e40446bd191ceb2c
alexandre <alexandre020602@gmail.com>
2025-12-27 14:40:12 -0300

    prodcut page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import ProductsList from "@/components/ProductsList/ProductsList"
+import { stripe } from "@/lib/stripe"
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  })
+
+
   return (
-    <div>
-      products page
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">All Products</h1>
+      <ProductsList products={products.data} />
     </div>
   )
 }
