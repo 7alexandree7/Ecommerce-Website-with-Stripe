db00862a826dd3864ecdc050fb720194b5ec22c6
alexandre <alexandre020602@gmail.com>
2025-12-27 19:36:48 -0300

    adicionando o filtro de pesquisa pelo name e description

--- components/ProductsList/ProductsList.tsx
+++ components/ProductsList/ProductsList.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Stripe from 'stripe'
 import ProductCard from '../ProductCard/ProductCard'
 
@@ -7,11 +9,18 @@ interface Props {
 }
 
 const ProductsList = ({ products }: Props) => {
+
+    const [searchTerm, setSearchTerm] = useState<string>("")
+    const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        || (product.description?.toLowerCase().includes(searchTerm.toLowerCase()) ?? false))
+
     return (
         <div>
             <div className="mb-6 flex justify-center">
                 <input
                     type="text"
+                    value={searchTerm}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
                     placeholder="Search products..."
                     required
                     className='w-full max-w-md rounded-md border border-gray-300 px-4 py-2 focus:outline-none'
@@ -19,9 +28,9 @@ const ProductsList = ({ products }: Props) => {
             </div>
 
             <ul className='mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
-                {products.map((product, index) => {
+                {filteredProducts.map((product) => {
                     return (
-                        <li key={index}> <ProductCard product={product} /> </li>
+                        <li key={product.id}> <ProductCard product={product} /> </li>
                     )
                 })}
             </ul>
